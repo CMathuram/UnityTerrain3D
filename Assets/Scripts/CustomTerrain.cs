e60080d47aebd0b7f59e9736ee5df016e39290e4
SIMBA\Chris <Christian.Mathuram@hotmail.com>
2019-02-13 21:57:38 +0530

    Player Controls and AI
    Player controls and AI. With nav mesh

---
+++ Assets/Scripts/CustomTerrain.cs
@@ -0,0 +1,53 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+[ExecuteInEditMode]
+
+public class CustomTerrain : MonoBehaviour {
+
+    //void Awake()
+    //{
+    //    SerializedObject tagManager = new SerializedObject(
+    //        AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+    //    SerializedProperty tagProp = tagManager.FindProperty("tags");
+
+    //    AddTag(tagProp, "Terrain");
+    //    AddTag(tagProp, "Cloud");
+    //    AddTag(tagProp, "Shore");
+
+    //    tagManager.ApplyModifiedProperties();
+
+    //    this.gameObject.tag = "Terrain";
+    //}
+
+    //void AddTag(SerializedProperty tagProp, string newTag)
+    //{
+    //    bool found = false;
+
+    //    for (int i = 0; i < tagProp.arraySize; i++)
+    //    {
+    //        SerializedProperty t = tagProp.GetArrayElementAtIndex(i);
+    //        if (t.stringValue.Equals(newTag)) { found = true; break; }
+    //    }
+
+    //    if(!found)
+    //    {
+    //        tagProp.InsertArrayElementAtIndex(0);
+    //        SerializedProperty newTagProp = tagProp.GetArrayElementAtIndex(0);
+    //        newTagProp.stringValue = newTag;
+    //    }
+    //}
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/CustomTerrainEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+//using EditorGUITable;
+
+//[CustomEditor(typeof(CustomTerrain))]
+//[CanEditMultipleObjects]
+
+public class CustomTerrainEditor : MonoBehaviour {
+
+    void OnEnable()
+    {
+        
+    }
+
+  //  public override void OnInspectorGUI()
+    //{
+
+    //}
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
